dfd9dcd1a1df8ab507561cd7464e54c1f4a6b4eb
Mihir Singh <git.service@mihirsingh.com>
2015-10-23 10:38:52 -0400

    Added support for parsing and building the StatusPage.io metrics

--- gullinkambi/gullinkambi.py
+++ gullinkambi/gullinkambi.py
@@ -3,6 +3,9 @@
 
 import json
 import os.path
+import gullinkambi.sumologic
+import gullinkambi.statuspageio
+import gullinkambi.metric
 
 
 class Gullinkambi(object):
@@ -14,3 +17,20 @@ class Gullinkambi(object):
 
         with open(config_path, 'r') as f:
             self.config = json.load(f)
+
+        sumologic_client = gullinkambi.sumologic.Client(
+            **self.config['auth']['sumologic'])
+
+        statuspageio_client = gullinkambi.statuspageio.Client(
+            **self.config['auth']['statuspageio'])
+
+        self.metrics = []
+
+        for metric in self.config['metrics']:
+            self.metrics.append(gullinkambi.metric.Metric(**{
+                'metric_id': metric['id'],
+                'query': metric['query'],
+                'sumologic_client': sumologic_client,
+                'statuspageio_client': statuspageio_client,
+                'interval': metric['interval']
+            }))
