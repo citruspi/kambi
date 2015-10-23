cb75ab7cc00d1b352c372acb6f874b85a2688f17
Mihir Singh <git.service@mihirsingh.com>
2015-10-23 05:34:08 -0400

    Implemented a bare bones Sumo Logic client

---
+++ gullinkambi/sumologic.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import requests
+
+
+class Client(object):
+
+    def __init__(self, user, token):
+        self.conn = requests.Session()
+        self.conn.auth = (user, token)
+        self.con.headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+    def search(self, query, from_timestamp=None, to_timestamp=None):
+        from_timestamp = from_timestamp or int(time.time() * 1000)-900000
+        to_timestamp = to_timestamp or int(time.time() * 1000)
+
+        endpoint = 'https://api.us2.sumologic.com/api/v1/logs/search'
+
+        params = {
+            'q': query,
+            'from': from_timestamp,
+            'to': to_timestamp,
+            'tz': 'Etc/UTC',
+            'format': 'json'
+        }
+
+        r = self.conn.get(endpoint, params=params)
+
+        r.raise_for_status()
+
+        return r.json()
