ac79e53c7585a749eb85a4e30a400104facd4a3a
Mihir Singh <git.service@mihirsingh.com>
2015-10-23 05:55:59 -0400

    Implemented a bare bones client for StatusPage.io

---
+++ gullinkambi/statuspageio.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import requests
+
+
+class Client(object):
+
+    def __init__(self, token, page_id):
+        self.conn = requests.Session()
+        self.conn.headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'OAuth {}'.format(token)
+        }
+
+        self.page_id = page_id
+
+    def post_metric_data(self, metric_id, data, keys=None):
+        keys = keys or {
+            'timestamp': '_timeslice',
+            'value': 'value'
+        }
+
+        endpoint = 'https://api.statuspage.io/v1/pages/{page}/metrics/' \
+            '{metric}/data.json'
+        endpoint = endpoint.format(page=self.page_id, metric=metric_id)
+
+        for point in data:
+            backoff = 1
+
+            data = {
+                'data[timestamp]': point[keys['timestamp']]/1000,
+                'data[value]': point[keys['value']]
+            }
+
+            while True:
+                r = self.conn.post(endpoint, data=data)
+
+                try:
+                    r.raise_for_status()
+                    break
+                except Exception as e:
+                    if r.status_code == 420:
+                        time.sleep(backoff)
+                        backoff += backoff
+                    else:
+                        raise e
