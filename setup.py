51865fa0406a1e0cdc8d251647a4ab3aa1a98399
Mihir Singh <git.service@mihirsingh.com>
2015-10-26 23:12:36 -0400

    Added kambi.stores to the list of packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     author='Mihir Singh (@citruspi)',
     author_email='mihir.singh@hudl.com',
-    packages=['kambi'],
+    packages=['kambi', 'kambi.stores'],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
